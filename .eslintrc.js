8904361b2d010d26095efe13a88b93dc2d7645e2
Kevin <sxzz@sxzz.moe>
2022-01-19 23:49:00 +0800

    chore: eslint ignore tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('eslint-define-config')
 
 module.exports = defineConfig({
+  root: true,
   extends: ['@sxzz/eslint-config-vue', '@sxzz/eslint-config-prettier'],
   rules: {
     'import/named': 'off',
