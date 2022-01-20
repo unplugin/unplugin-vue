3de64a54310fb2340867608dd5dd3c80f38ea1ea
Kevin <sxzz@sxzz.moe>
2022-01-20 16:10:29 +0800

    feat: add webpack example

---
+++ examples/webpack/webpack.config.ts
@@ -0,0 +1,18 @@
+import Vue from 'unplugin-vue/webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import type { Configuration } from 'webpack'
+
+const config: Configuration = {
+  mode: (process.env.MODE as any) ?? 'development',
+  entry: {
+    app: './src/main.ts',
+  },
+  plugins: [
+    Vue(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index.html',
+    }),
+  ],
+}
+export default config
