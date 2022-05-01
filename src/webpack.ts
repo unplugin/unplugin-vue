a60fb2de414dde71315d568d725173dd7d0a19c1
三咲智子 <sxzz@sxzz.moe>
2022-05-01 15:35:14 +0800

    fix: webpack types

--- src/webpack.ts
+++ src/webpack.ts
@@ -1,3 +1,6 @@
 import unplugin from '.'
+// @ts-expect-error fix issue #42873
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { WebpackPluginInstance } from 'webpack'
 
 export default unplugin.webpack
