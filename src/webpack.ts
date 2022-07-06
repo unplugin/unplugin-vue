02657feed5a3ded268f4b3936e9975d3416fc2e0
三咲智子 <sxzz@sxzz.moe>
2022-07-07 01:26:14 +0800

    chore: upgrade deps

--- src/webpack.ts
+++ src/webpack.ts
@@ -1,6 +1,4 @@
 import unplugin from '.'
-// @ts-expect-error fix issue #42873
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import type { WebpackPluginInstance } from 'webpack'
 
-export default unplugin.webpack
+export default unplugin.webpack as WebpackPluginInstance
