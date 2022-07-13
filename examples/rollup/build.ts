808f1426960ccd9b61c88106a453ae49ede1740f
三咲智子 <sxzz@sxzz.moe>
2022-07-13 23:14:17 +0800

    fix(examples): rollup build

--- examples/rollup/build.ts
+++ examples/rollup/build.ts
@@ -1,10 +1,11 @@
 import { rollup } from 'rollup'
 import Vue from 'unplugin-vue/rollup'
+import esbuild from 'rollup-plugin-esbuild'
 ;(async () => {
   const bundle = await rollup({
     input: ['./src/main.ts'],
     external: ['vue'],
-    plugins: [Vue()],
+    plugins: [Vue(), esbuild({ format: 'esm' })],
   })
   await bundle.write({
     dir: 'dist',
