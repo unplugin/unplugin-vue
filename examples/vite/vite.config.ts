d461d46a71e87a7ac7ef8d842a100d407cd9d7c3
三咲智子 <sxzz@sxzz.moe>
2022-08-18 18:53:41 +0800

    chore(examples): add inspect

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import Vue from 'unplugin-vue/vite'
+import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
-  plugins: [Vue()],
+  plugins: [Vue(), Inspect()],
 })
