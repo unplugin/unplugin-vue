5cfe35ae67a64f02eec738a7d44ee0c2abf2fd09
三咲智子 <sxzz@sxzz.moe>
2022-10-03 22:12:18 +0800

    fix: plugin name in error message

--- src/core/compiler.ts
+++ src/core/compiler.ts
@@ -16,7 +16,7 @@ export function resolveCompiler(root: string): typeof _compiler {
   if (!compiler) {
     throw new Error(
       `Failed to resolve vue/compiler-sfc.\n` +
-        `@vitejs/plugin-vue requires vue (>=3.2.25) ` +
+        `unplugin-vue requires vue (>=3.2.25) ` +
         `to be present in the dependency tree.`
     )
   }
