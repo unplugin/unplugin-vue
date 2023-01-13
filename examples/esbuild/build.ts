09f1815aaab8fbd6c91b9d50cc6c19e70ec42500
neo.st.fox <neo.st.fox@outlook.com>
2023-01-13 14:35:52 +0800

    esbuild scss 问题最小复现单元

--- examples/esbuild/build.ts
+++ examples/esbuild/build.ts
@@ -1,14 +1,21 @@
 import { build } from 'esbuild'
 import Vue from 'unplugin-vue/esbuild'
-;(async () => {
-  await build({
-    entryPoints: ['src/main.ts'],
-    bundle: true,
-    outfile: 'dist/main.js',
-    format: 'esm',
-    define: {
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    },
-    plugins: [Vue()],
-  })
-})()
+  ; (async () => {
+    await build({
+      entryPoints: ['src/main.ts'],
+      bundle: true,
+      outfile: 'dist/main.js',
+      format: 'esm',
+      define: {
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      },
+      plugins: [
+        Vue({
+          style: {
+            // @ts-ignore
+            preprocessLang: 'scss',
+          },
+        }),
+      ],
+    })
+  })()
