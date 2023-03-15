c9a8850e8861c15a9fb7a3ee7766080171b14130
neo.st.fox <neo.st.fox@outlook.com>
2023-03-15 16:10:01 +0800

    fix(core): #92 scss source map error and run tests

--- src/core/style.ts
+++ src/core/style.ts
@@ -26,14 +26,14 @@ export async function transformStyle(
     scoped: block.scoped,
     ...(options.cssDevSourcemap
       ? {
-        postcssOptions: {
-          map: {
-            from: filename,
-            inline: false,
-            annotation: false,
+          postcssOptions: {
+            map: {
+              from: filename,
+              inline: false,
+              annotation: false,
+            },
           },
-        },
-      }
+        }
       : {}),
   })
 
@@ -53,11 +53,11 @@ export async function transformStyle(
 
   const map = result.map
     ? await formatPostcssSourceMap(
-      // version property of result.map is declared as string
-      // but actually it is a number
-      result.map as Omit<RawSourceMap, 'version'> as ExistingRawSourceMap,
-      filename
-    )
+        // version property of result.map is declared as string
+        // but actually it is a number
+        result.map as Omit<RawSourceMap, 'version'> as ExistingRawSourceMap,
+        filename
+      )
     : (null as any)
 
   return {
