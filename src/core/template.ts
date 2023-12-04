50a2928c1bc9920079a79573e41f6ee35e275098
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-12-04 19:22:04 +0800

    fix: respect vite base config for absolute URLs during dev
    ref: https://github.com/vitejs/vite-plugin-vue/commit/44d3eb0a619fa1f512b59c8ed1277aa28fe9e2b6

--- src/core/template.ts
+++ src/core/template.ts
@@ -124,6 +124,7 @@ export function resolveTemplateCompilerOptions(
           (options.devServer.config.server?.origin ?? '') +
           devBase +
           slash(path.relative(options.root, path.dirname(filename))),
+        includeAbsolute: !!devBase,
       }
     }
   } else if (transformAssetUrls !== false) {
