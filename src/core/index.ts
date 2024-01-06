61410d6571385386298979fb118a0754db913c7d
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-01-07 04:10:59 +0800

    refactor: deprecate defineModel option
    ref: https://github.com/vitejs/vite-plugin-vue/commit/0d52a27774ab58af6be257707a1984d0cba4f1bd

--- src/core/index.ts
+++ src/core/index.ts
@@ -51,7 +51,13 @@ export interface Options {
       | 'fs'
       | 'hoistStatic'
     >
-  >
+  > & {
+    /**
+     * @deprecated defineModel is now a stable feature and always enabled if
+     * using Vue 3.4 or above.
+     */
+    defineModel?: boolean
+  }
   template?: Partial<
     Pick<
       SFCTemplateCompileOptions,
