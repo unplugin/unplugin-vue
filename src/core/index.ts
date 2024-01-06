d67c67a88d59daadd188b3547c64b0bc40ee22ee
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-01-07 04:10:20 +0800

    feat: provide default value for __VUE_PROD_HYDRATION_MISMATCH_DETAILS__
    ref: https://github.com/vitejs/vite-plugin-vue/commit/0fbc4a4f8a9bfb193a28a02ef9e25a06631426a9

--- src/core/index.ts
+++ src/core/index.ts
@@ -179,6 +179,8 @@ export const plugin = createUnplugin<Options | undefined, false>(
               __VUE_OPTIONS_API__: config.define?.__VUE_OPTIONS_API__ ?? true,
               __VUE_PROD_DEVTOOLS__:
                 config.define?.__VUE_PROD_DEVTOOLS__ ?? false,
+              __VUE_PROD_HYDRATION_MISMATCH_DETAILS__:
+                config.define?.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ ?? false,
             },
             ssr: {
               // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
