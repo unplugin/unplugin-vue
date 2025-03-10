735cb52542f6be508034c017f06ac12e00b94c69
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-11 06:02:58 +0800

    chore: upgrade deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,3 @@
 import { sxzz } from '@sxzz/eslint-config'
 
-export default sxzz({
-  rules: {
-    'unicorn/filename-case': 'off',
-  },
-})
+export default sxzz().removeRules('unicorn/filename-case')
