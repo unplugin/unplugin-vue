56da195dd8b9bddc9dc14e2128800ef2fa6792c8
Kevin Deng <sxzz@sxzz.moe>
2025-08-30 18:50:35 +0800

    chore: simplify tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src/*.ts'],
-  format: 'esm',
-  target: 'node18',
-  clean: true,
-  dts: true,
+  exports: true,
   shims: true,
 })
